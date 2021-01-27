d954ed54bfdaeb43ac9e45a068d723d77ccf1bb9
Chad Scherrer <chad.scherrer@gmail.com>
2021-01-27 13:26:56 -0800

    Delete __site directory

--- __site/assets/blog/2021/01-25-symbolic-simplification/code/ex13.jl
+++
@@ -1,7 +0,0 @@
-# This file was generated, do not modify it. # hide
-before = @elapsed dynamicHMC(post)
-after = @elapsed dynamicHMC(post; ℓ=ℓ)
-
-speedup = before / after
-
-@show before, after, speedup
