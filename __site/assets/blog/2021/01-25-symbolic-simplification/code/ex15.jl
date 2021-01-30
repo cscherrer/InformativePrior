2f40bd2db1aeb0d8f2088ccd29c68911e9f7af64
Chad Scherrer <chad.scherrer@gmail.com>
2021-01-29 16:09:32 -0800

    Delete __site directory

--- __site/assets/blog/2021/01-25-symbolic-simplification/code/ex15.jl
+++
@@ -1,4 +0,0 @@
-# This file was generated, do not modify it. # hide
-t_inference = print(round(@elapsed begin #hide
-postsample = dynamicHMC(post; ℓ=ℓ)
-end; sigdigits=4)) #hide
--- __site/assets/blog/2021/01-28-measure-theory/code/_ceval_3.jl
+++
@@ -1,2 +0,0 @@
-# This file was generated, do not modify it. # hide
-logdensity(Normal(), Lebesgue(ℝ), 1.0)
